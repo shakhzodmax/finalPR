6660bef05cdbe3a197f81701219c72cc1c71f7ae
dilmurod <dilmurodmusulmonov6@gmail.com>
2022-11-03 17:25:51 +0500

    feat: add depoService & itService

---
+++ src/views/DepoServicePage.vue
@@ -0,0 +1,88 @@
+<template>  
+  <div class="main h-mainPng bg-no-repeat bg-center">
+    <div class="container mx-auto text-white text-center">
+      <div class="pt-mainPt text-4xl font-black mb-1">Наши услуги</div>
+      <div>Мы предоставляем надежные финансовые услуги, постоянно проводя преобразования и инновации</div>
+      <div class="h-14 bg-sky-600  flex justify-start items-center px-8 rounded-md 2xl:visible 2xl:mt-24 xl:mt-24 xl:visible lg:visible lg:mt-24 md:mt-16 md:visible sm:invisible invisible ">
+        <nav class="flex px-5 py-3 text-white" aria-label="Breadcrumb">
+          <ol class="inline-flex items-center space-x-1 md:space-x-10">
+            <li class="inline-flex items-center">
+              <a href="#" class="inline-flex items-center text-md font-medium hover:text-slate-300">
+                <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                Главный экран
+              </a>
+            </li>
+            <li>
+              <div class="flex items-center">
+                <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                <span class="ml-1 text-md font-medium">Наши услуги</span>
+              </div>
+            </li>
+            <li aria-current="page">
+              <div class="flex items-center">
+                <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                <span class="ml-1 text-md font-medium">Депозитарные услуги</span>
+              </div>
+            </li>
+          </ol>
+        </nav>
+      </div>
+    </div>
+  </div>
+  <div class="header">
+    <div class="container mx-auto">
+      <div class="container mx-auto">
+        <div class="flex justify-center items-end mt-20">
+          <div class="flex flex-col">
+            <div class="flex justify-end">
+              <div class="w-3 h-1 bg-gray-400"></div>
+              <div class="w-5 h-1 bg-green-400"></div>
+              <div class="w-3 h-1 bg-sky-400"></div>
+            </div>
+            <div class="text-2xl font-bold mb-16">Депозитарные услуги</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <div class="divider bg-sky-700 py-8">
+    <div class="container mx-auto">
+      <div class="text-center">
+        <p class="text-xl text-white text-justify xl:text-justify lg:text-justify md:text-justify 2xl:text-justify 2xl:px-32 xl:ml-8 xl:mr-8 lg:ml-10 lg:pr-10 md:pl-10 sm:pl-10 sm: pl-2 md:pr-10 sm:mr-10 ">Центральный депозитарий осуществляет свою деятельность в соответствии с Законом Республики Узбекистан «О рынке ценных бумаг» и иными законодательными нормами Республики. Статьями 39 и 40 указанного Закона устанавливается перечень исключительных функций, в том числе права и обязательства Центрального депозитария.</p>
+      </div>
+      </div>
+  </div>
+  <div class="coontainer mx-auto mb-16">
+    <div class="container mx-auto  text-center">
+      <p class="text-xl  font-bold  text-center mt-10  sm: w-full -ml-1">В соответствии с Законом Республики Узбекистан «О рынке ценных бумаг» Центральному депозитарию ценных бумаг возложены:
+</p>
+<ul class="list-disc xl:text-justify lg:text-justify md:text-justify 2xl:text-justify 2xl:px-32 xl:ml-10 mr-10 lg:ml-10 pr-10  md:pl-10 md:pr-10 sm:pl-10 md:ml-8 sm:ml-2">
+  <li class="text-xl mt-10 ">постановка на учет ценных бумаг;</li>
+  <li class="text-xl mt-2">хранение документарных ценных бумаг;</li>
+  <li class="text-xl mt-2">хранение документов, подтверждающих выпуск ценных бумаг;</li>
+  <li class="text-xl mt-2">ведение реестров владельцев ценных бумаг;</li>
+  <li class="text-xl mt-2">учет прав государства на ценные бумаги, а также прав на ценные бумаги уполномоченных государством лиц по управлению ценными бумагами;</li>
+  <li class="text-xl mt-2">ведение счетов депо инвестиционных посредников и иностранных номинальных держателей;</li>
+  <li class="text-xl mt-2">учет допущенных к обращению на территории Республики Узбекистан ценных бумаг, выпущенных нерезидентами Республики Узбекистан;</li>
+  <li class="text-xl mt-2">учет допущенных к обращению вне территории Республики Узбекистан ценных бумаг, выпущенных резидентами Республики Узбекистан;</li>
+  <li class="text-xl mt-2">учет прав на ценные бумаги, выпущенные и принадлежащие инвестиционным фондам, если иное не установлено законодательством;</li>
+
+  <li class="text-xl mt-2">ведение Единой базы депонентов;</li>
+  <li class="text-xl mt-2"> присвоение международных кодов эмиссионным ценным бумагам, выпущенным резидентами Республики Узбекистан;</li>
+  <li class="text-xl mt-2">сбор и систематизация информации о движении эмиссионных ценных бумаг по счетам депо.</li>
+</ul>
+</div>
+  </div>
+</template>
+
+<script>
+</script>
+
+<style>
+.main{
+  background: url("@/assets/aboutUs.png");
+}
+*{
+  font-family: "Gotham Pro", Gotham, Arial;
+}
+</style>
---
+++ src/views/ItServicePage.vue
@@ -0,0 +1,73 @@
+<template>  
+    <div class="main h-mainPng bg-no-repeat bg-center">
+      <div class="container mx-auto text-white text-center">
+        <div class="pt-mainPt text-4xl font-black mb-1">Наши услуги</div>
+        <div>Мы предоставляем надежные финансовые услуги, постоянно проводя преобразования и инновации</div>
+        <div class="h-14 bg-sky-600  flex justify-start items-center px-8 rounded-md 2xl:visible 2xl:mt-24 xl:mt-24 xl:visible lg:visible lg:mt-24 md:mt-16 md:visible sm:invisible invisible ">
+          <nav class="flex px-5 py-3 text-white" aria-label="Breadcrumb">
+            <ol class="inline-flex items-center space-x-1 md:space-x-10">
+              <li class="inline-flex items-center">
+                <a href="#" class="inline-flex items-center text-md font-medium hover:text-slate-300">
+                  <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                  Главный экран
+                </a>
+              </li>
+              <li> 
+                <div class="flex items-center">
+                  <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                  <span class="ml-1 text-md font-medium">Наши услуги</span>
+                </div>
+              </li>
+              <li aria-current="page">
+                <div class="flex items-center">
+                  <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                  <span class="ml-1 text-md font-medium">Информационные услуги</span>
+                </div>
+              </li>
+            </ol>
+          </nav>
+        </div>
+      </div>
+    </div>
+    <div class="header">
+      <div class="container mx-auto">
+        <div class="container mx-auto">
+          <div class="flex justify-center items-end mt-20">
+            <div class="flex flex-col">
+              <div class="flex justify-end">
+                <div class="w-3 h-1 bg-gray-400"></div>
+                <div class="w-5 h-1 bg-green-400"></div>
+                <div class="w-3 h-1 bg-sky-400"></div>
+              </div>
+              <div class="text-2xl font-bold mb-16">Информационные услуги</div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="divider bg-sky-700 py-8">
+      <div class="container mx-auto">
+        <div class="text-center">
+          <p class="text-xl text-white text-justify xl:text-justify lg:text-justify md:text-justify 2xl:text-justify 2xl:px-32 xl:ml-8 xl:mr-8 lg:ml-10 lg:pr-10 md:pl-10 sm:pl-10 sm: pl-2 md:pr-10 sm:mr-10 md:ml-22 sm:text-justify ml-2 mr-4">Центральный депозитарий осуществляет свою деятельность согласно Закону Республики Узбекистан “О рынке ценных бумаг” и других законодательных норм республики. Статья 39 и 40 вышеназванного Закона устанавливает перечень исключительных функций, и в том числе права и обязанности Центрального депозитария ценных бумаг.</p>
+        </div>
+        </div>
+    </div>
+    <div class="coontainer mx-auto mb-16">
+      <div class="container mx-auto  text-center md:text-justify">
+        <p class="text-xl text-black 2xl:mt-10 lg:mt-10 md:mt-10 sm:mt-10 text-justify xl:text-justify lg:text-justify md:text-justify 2xl:text-justify 2xl:px-32 xl:ml-8 xl:mr-8 lg:ml-10 lg:pr-10 md:pl-10 sm:pl-10 sm: pl-2 md:pr-10 sm:mr-10 ml-2 mr-4 mt-6">Центральный Депозитарий в соответствии с «Положением о порядке взаимодействия судебных исполнителей с судами, правоохранительными, регистрирующими органами, финансово-кредитными организациями при принудительном исполнении судебных актов и актов иных органов» (утверждено Приказом министра юстиции от 05.02.2007 г. N 21-мх, зарегистрированным МЮ 05.02.2007 г. N 1656) оказывает информационные услуги уполномоченным государственным органам по выдаче информации о наличии ценных бумаг на счетах депо должников.</p>
+        <p class="text-xl text-black 2xl:mt-4 md:mt-6  sm:mt-6   text-justify xl:text-justify lg:text-justify md:text-justify 2xl:text-justify 2xl:px-32 xl:ml-8 xl:mr-8 lg:ml-10 lg:pr-10 md:pl-10 sm:pl-10 sm: pl-2 md:pr-10 sm:mr-10 mt-6 mr-2">Кроме этого, на сайте Центрального депозитария еженедельно размещается статистическая информация по учету и хранению ценных бумаг, выпущенных эмитентами.</p>
+  </div>
+    </div>
+  </template>
+  
+  <script>
+  </script>       
+  
+  <style>
+  .main{
+    background: url("@/assets/aboutUs.png");
+  }
+  *{
+    font-family: "Gotham Pro", Gotham, Arial;
+  }
+  </style>   
