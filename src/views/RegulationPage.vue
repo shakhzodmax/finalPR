aa0a6a4b78eb9727c3f73122cd09d8e3a610b6ea
mirjon <mirjon0603@gmail.com>
2022-11-03 11:58:36 +0500

    feat: add pages

---
+++ src/views/RegulationPage.vue
@@ -0,0 +1,103 @@
+<template>
+  <div class="main h-mainPng bg-no-repeat bg-center">
+    <div class="container mx-auto text-white text-center">
+      <div class="pt-mainPt  font-black mb-1 2xl:text-4xl xl:text-4xl lg:text-4xl md:text-4xl text-xl ">Корпоративное управление</div>
+      <div class="2xl:text-xl xl:text-xl lg:text-xl md:text-xl text-xs">Финансовая инвестиционная инфраструктура, поддерживающая рост рынка капитала Республики Узбекистан</div>
+      <div class="h-14 bg-sky-600 mt-24 flex justify-start items-center px-8 2xl:visible xl:visible lg:visible md:visible sm:invisible invisible">
+        <nav class="flex px-5 py-3 text-white" aria-label="Breadcrumb">
+          <ol class="inline-flex items-center space-x-1 md:space-x-10">
+            <li class="inline-flex items-center">
+              <a href="#" class="inline-flex items-center text-md font-medium hover:text-slate-300">
+                <svg class="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10.707 2.293a1 1 0 00-1.414 0l-7 7a1 1 0 001.414 1.414L4 10.414V17a1 1 0 001 1h2a1 1 0 001-1v-2a1 1 0 011-1h2a1 1 0 011 1v2a1 1 0 001 1h2a1 1 0 001-1v-6.586l.293.293a1 1 0 001.414-1.414l-7-7z"></path></svg>
+                Корпоративное управление 
+              </a>
+            </li>
+            <li>
+              <div class="flex items-center">
+                <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                <span class="ml-1 text-md font-medium">Документы</span>
+              </div>
+            </li>
+            <li aria-current="page">
+              <div class="flex items-center">
+                <svg class="w-6 h-6 text-white" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd"></path></svg>
+                <span class="ml-1 text-md font-medium">Устав</span>   
+              </div>
+            </li>
+          </ol>
+        </nav>
+      </div>
+    </div>
+  </div>
+  <div class="header">
+    <div class="container mx-auto">
+      <div class="container mx-auto">
+        <div class="flex justify-center items-end mt-20">
+          <div class="flex flex-col">
+            <div class="flex justify-end">
+              <div class="w-3 h-1 bg-gray-400"></div>
+              <div class="w-5 h-1 bg-green-400"></div>
+              <div class="w-3 h-1 bg-sky-400"></div>
+            </div>
+            <div class="2xl:text-2xl xl:text-2xl lg:text-2xl md:text-2xl text-xl font-bold ">Устав</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+ 
+
+ 
+    <div class="container mx-auto text-justify mt-24 mb-12">
+   
+      <div class="overflow-x-auto relative">
+    <table class="w-full text-sm text-left text-gray-500 dark:text-gray-400">
+        <thead class="text-xs text-center text-gray-700 uppercase bg-gray-100 dark:bg-gray-700 dark:text-gray-400">
+            <tr>
+                <th scope="col" class="py-3 px-6 rounded-l-lg">
+                  Устав
+                </th>
+                <th scope="col" colspan="2" class="py-3 px-6">
+                   Загрузить
+                </th>
+               </tr>
+        </thead>
+        <tbody class="">
+            <tr class="bg-white dark:bg-gray-800 ">
+                <th scope="row" class="py-4 px-6 font-medium text-gray-900 whitespace-nowrap dark:text-white flex justify-center">
+                  19.03.2019 60-п “устав центрального депозитария ценных бумаг”    
+                </th>
+                <td class="py-4 px-6 ">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                  <path fill-rule="evenodd" d="M5.625 1.5H9a3.75 3.75 0 013.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 013.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 01-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875zm5.845 17.03a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V12a.75.75 0 00-1.5 0v4.19l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3z" clip-rule="evenodd" />
+                  <path d="M14.25 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0016.5 7.5h-1.875a.375.375 0 01-.375-.375V5.25z" />
+                  </svg>
+                  <p>.pdf</p>
+                </td>
+                <td class="py-4 px-6">
+                  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="w-6 h-6">
+                  <path fill-rule="evenodd" d="M5.625 1.5H9a3.75 3.75 0 013.75 3.75v1.875c0 1.036.84 1.875 1.875 1.875H16.5a3.75 3.75 0 013.75 3.75v7.875c0 1.035-.84 1.875-1.875 1.875H5.625a1.875 1.875 0 01-1.875-1.875V3.375c0-1.036.84-1.875 1.875-1.875zm5.845 17.03a.75.75 0 001.06 0l3-3a.75.75 0 10-1.06-1.06l-1.72 1.72V12a.75.75 0 00-1.5 0v4.19l-1.72-1.72a.75.75 0 00-1.06 1.06l3 3z" clip-rule="evenodd" />
+                  <path d="M14.25 5.25a5.23 5.23 0 00-1.279-3.434 9.768 9.768 0 016.963 6.963A5.23 5.23 0 0016.5 7.5h-1.875a.375.375 0 01-.375-.375V5.25z" />
+                 </svg>
+                   <p>.docx</p>
+                 </td>
+                 </tr>
+              </tbody>  
+    </table>
+</div>
+</div>
+    
+  
+</template>
+
+<script>
+</script>
+
+<style>
+.main{
+  background: url("@/assets/aboutUs.png");
+}
+*{
+  font-family: "Gotham Pro", Gotham, Arial;
+}
+</style>
